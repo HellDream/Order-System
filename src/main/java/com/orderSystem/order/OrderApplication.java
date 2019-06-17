31befaedfdb52ecdcd85a29c13c3376766deca3e
yzy <673598755@qq.com>
2019-06-18 00:01:34 +0800

    project initialize

---
+++ src/main/java/com/orderSystem/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.orderSystem.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
